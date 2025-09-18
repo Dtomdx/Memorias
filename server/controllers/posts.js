d05096363ae2fb1bc02fb71ef4233d4d89a79287
tom <tom@protonmail.com>
2025-09-17 19:05:08 -0500

    S: Estructura base express

---
+++ server/controllers/posts.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
+
+
+
+export const getPosts = async(req, res) => {
+    try {
+        const postMessage = await PostMessage.find()
+        res.status(200).json(postMessage)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
