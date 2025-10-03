4f612a5ae1e5fcdc7bf7d33d8a3e3820e1a471ff
tom <tom@protonmail.com>
2025-10-02 19:05:54 -0500

    S: Middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken"
+
+const secret = process.env.SECRET_TOKEN;
+
+const auth = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if (!req.headers.authorization){
+            return res.status(401).json({message: "Token requerido"})
+        }
+        const isCustomAuth = token.length < 500;
+
+        let decodedData;
+
+        if(token && isCustomAuth){
+            //sign
+            decodedData = jwt.verify(token, secret);
+            req.userId = decodedData?.id
+        } else {
+            //google
+            decodedData = jwt.decode(token);
+            req.userId = decodedData?.sub;
+        }
+        next()
+    } catch (error) {
+        res.status(401).json({message: "No autorizado"})
+    }
+}
+
+export default auth;
